67e10a7483dd6122888e77c01fadff425dd6d944
pedro00876 <pcchagas00@gmail.com>
2026-05-19 21:28:49 -0300

    refactor: restructure repository to keep only frontend at the root level

--- backend/src/main/java/com/squad20/sistema_climbe/exception/PendingApprovalException.java
+++
@@ -1,7 +0,0 @@
-package com.squad20.sistema_climbe.exception;
-
-public class PendingApprovalException extends RuntimeException {
-    public PendingApprovalException(String message) {
-        super(message);
-    }
-}
